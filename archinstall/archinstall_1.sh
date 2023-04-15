cf1a57535c41a3a66726f1380809fb0595b5311c
MrDisguised <128170542+MrDisguised@users.noreply.github.com>
2023-04-15 10:18:34 +0000

    Update archinstall_1.sh

--- archinstall/archinstall_1.sh
+++ archinstall/archinstall_1.sh
@@ -1,7 +1,22 @@
 timedatectl
 timedatectl set-ntp true
-echo Now parition your drive - sda
+echo Now parition your drives sda and sdb
 sda1 - 512M - Will be mounted on /boot
 sda2 - 4G - Will be a [SWAP] partition
 sda3 - Rest - Will be mounted on /mnt
-read -p "Do you want to partition your drives? Answer in (yes/no)" answer
+sleep 7
+read -p "Do you want to partition your drives? Answer in (yes/no). Type the full word (yes/no). DON'T TYPE (Y/N) " answer
+if [ "$answer" = "yes" ]; then
+  
+  echo "First edit your sda drive"
+  echo "Opening cfdisk"
+  sleep 3
+  cfdisk /dev/sda
+  echo "Now edit your sdb drive"
+  echo "Opening cfdisk"
+  sleep 3
+  cfdisk /dev/sdb
+else
+  curl | sh
+
+  
