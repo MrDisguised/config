dcdce46cfaec799b98a7df9d322cea34111bb097
MrDisguised <128170542+MrDisguised@users.noreply.github.com>
2023-04-18 06:26:46 +0000

    Create arkenfox-installer.sh

---
+++ arkenfox_firefox/arkenfox-installer.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+killall -q firefox
+rm -rf /home/$(whoami)/.mozilla/firefox/*.default-release/*
+rm -rf /home/$(whoami)/.cache/mozilla/firefox/*.default-release/*
+rm -rf /home/$(whoami)/.mozilla/firefox/*.default/*
+rm -rf /home/$(whoami)/.cache/mozilla/firefox/*.default/*
+cd ~/Desktop && git clone https://github.com/arkenfox/user.js && cd user.js
+cp -r * /home/rounak/.mozilla/firefox/ytrjqjqq.default-release/
+curl https://raw.githubusercontent.com/MrDisguised/config/main/arkenfox_firefox/user-overrides.txt > /home/$(whoami)/.mozilla/firefox/ytrjqjqq.default-release/user-overrides.js
+curl https://raw.githubusercontent.com/arkenfox/user.js/master/scratchpad-scripts/arkenfox-cleanup.js | wl-copy
+firefox about:config
+/home/$(whoami)/.mozilla/firefox/ytrjqjqq.default-release/updater.sh
+/home/$(whoami)/.mozilla/firefox/ytrjqjqq.default-release/prefsCleaner.sh
+rm -rf ~/Desktop/user.js
+echo "All good :)"
+
