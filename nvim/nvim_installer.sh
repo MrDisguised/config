6c66e622c953a5f6a748bc127fd68056f6003e31
MrDisguised <128170542+MrDisguised@users.noreply.github.com>
2023-04-18 06:48:40 +0000

    Update nvim_installer.sh

--- nvim/nvim_installer.sh
+++ nvim/nvim_installer.sh
@@ -3,3 +3,9 @@ echo "Installing vim-plug"
 sleep 2
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+echo "Vim Plug is installed"
+mkdir -p /home/$(whoami)/.config
+mkdir -p /home/$(whoami)/.config/nvim
+curl https://raw.githubusercontent.com/MrDisguised/config/main/nvim/init.nvim > /home/$(whoami)/.config/nvim/init.nvim
+echo "All done :)"
+echo "PLEASE MAKE SURE TO OPEN /home/username/.config/nvim/init.nvim and run :PlugInstall"
