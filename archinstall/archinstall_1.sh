9fab24a2fbc6ead6f90536ea3acfc0ee2b88d9ef
MrDisguised <128170542+MrDisguised@users.noreply.github.com>
2023-04-20 13:24:13 +0000

    fixed some terrible mistakes

--- archinstall/archinstall_1.sh
+++ archinstall/archinstall_1.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 timedatectl set-ntp true
-echo Now parition your drives sda and sdb
-sda1 - 512M - Will be mounted on /boot
-sda2 - 4G - Will be a [SWAP] partition
-sda3 - Rest - Will be mounted on /mnt
+echo "Now parition your drives sda and sdb"
+echo "sda1 - 512M - Will be mounted on /boot"
+echo "sda2 - 4G - Will be a [SWAP] partition"
+echo "sda3 - Rest - Will be mounted on /mnt"
 sleep 7
 read -p "Do you want to partition your drives? Answer in (yes/no). Type the full word (yes/no). DON'T TYPE (Y/N) " answer
 
@@ -16,8 +16,6 @@ if [ "$answer" = "yes" ]; then
   echo "Opening cfdisk"
   sleep 3
   cfdisk /dev/sdb
-else
-  curl https://raw.githubusercontent.com/MrDisguised/config/main/archinstall/archinstall_2.sh | bash
 fi
 
 curl https://raw.githubusercontent.com/MrDisguised/config/main/archinstall/archinstall_2.sh | bash
