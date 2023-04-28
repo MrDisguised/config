2aae0638b9fff44b58d6a02d91b66f261dac1052
MrDisguised <128170542+MrDisguised@users.noreply.github.com>
2023-04-28 18:15:33 +0000

    did some changes

--- archinstall/archinstall_1.sh
+++ archinstall/archinstall_1.sh
@@ -3,10 +3,10 @@ timedatectl set-ntp true
 lsblk
 echo "Now time to partition your drives"
 echo "Look at lsblk and tell me your drive names"
-echo "What is your 1st drives 1st partition name ? Enter the full path name starting with /dev/xxxxxx"
-read driveone
-echo "What is your 1st drives 2nd partition name ? Enter the full path name starting with /dev/xxxxxx"
-read drivetwo
+echo "What is your main drives name ?"
+read d1
+echo "What is your 2nd drives name"
+read d2
 sleep 7
 read -p "Do you want to partition your drives? Answer in (yes/no/y/Y/N/n) " answer
 
@@ -15,13 +15,13 @@ case "$answer" in
     
     echo "Opening cfdisk"
     sleep 3
-    cfdisk $driveone
+    cfdisk $d1
     
     sleep 3
-    cfdisk $drivetwo
+    cfdisk $d2
     ;;
   no|n|N|NO)
-    echo "Make up ur mind/ fix ur shit and then run this script"
+    echo "Make up ur mind/fix ur shit and then run this script"
     exit 1
     ;;
   *)
@@ -29,15 +29,24 @@ case "$answer" in
     exit 1
     ;;
 esac
+
+echo "Okay now tell me the drive where you want to install EFI partition"
+read d1p1
+
+echo "Okay now tell me the drive where you want to install swap partition"
+read d1p2
+
+echo "Okay now tell me the drive where you want to install your operating system"
+read d1p3
 
 read -p "Do you want btrfs ? If no then it will format the drive in ext4. Answer in (yes/no). Dont use caps (YES/NO). TYPE THE FULL WORD. DONT TYPE (y/n/Y/N)!!!!!" sex
 
 case "$sex" in
   yes|y|Y|YES)
-    mkfs.btrfs /dev/sda3
+    mkfs.btrfs $d1p3
     ;;
   no|n|N|NO)
-    mkfs.ext4 /dev/sda3
+    mkfs.ext4 $d1p3
     ;;
   *)
     echo "Invalid input. Please run the script again "
@@ -45,11 +54,11 @@ case "$sex" in
     ;;
 esac
 
-mkswap /dev/sda2
-mkfs.fat -F 32 /dev/sda1
-mount --mkdir /dev/sda3 /mnt
-mount --mkdir /dev/sda1 /mnt/boot
-swapon /dev/sda2
+mkswap $d1p2
+mkfs.fat -F 32 $d1p1
+mount --mkdir $d1p3 /mnt
+mount --mkdir $d1p1 /mnt/boot
+swapon $d1p2
 pacman -Syyu reflector
 
 
