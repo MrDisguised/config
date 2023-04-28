48392aad5a9e8b00ff2c4d1e92449de45bf5fa18
MrDisguised <128170542+MrDisguised@users.noreply.github.com>
2023-04-28 15:12:40 +0000

    updated my bash script

--- archinstall/archinstall_1.sh
+++ archinstall/archinstall_1.sh
@@ -5,18 +5,27 @@ echo "sda1 - 512M - Will be mounted on /boot"
 echo "sda2 - 4G - Will be a [SWAP] partition"
 echo "sda3 - Rest - Will be mounted on /mnt"
 sleep 7
-read -p "Do you want to partition your drives? Answer in (yes/no). Type the full word (yes/no). DON'T TYPE (Y/N) " answer
+read -p "Do you want to partition your drives? Answer in (yes/no/y/Y/N/n) " answer
 
-if [ "$answer" = "yes" ]; then
-  echo "First edit your sda drive"
-  echo "Opening cfdisk"
-  sleep 3
-  cfdisk /dev/sda
-  echo "Now edit your sdb drive"
-  echo "Opening cfdisk"
-  sleep 3
-  cfdisk /dev/sdb
-fi
+case "$answer" in
+  yes|y|Y|YES)
+    echo "First edit your sda drive"
+    echo "Opening cfdisk"
+    sleep 3
+    cfdisk /dev/sda
+    echo "Now edit your sdb drive"
+    sleep 3
+    cfdisk /dev/sdb
+    ;;
+  no|n|N|NO)
+    echo "Make up ur mind/ fix ur shit and then run this script"
+    exit 1
+    ;;
+  *)
+    echo "Invalid input. Please run the script again "
+    exit 1
+    ;;
+esac
 
 curl https://raw.githubusercontent.com/MrDisguised/config/main/archinstall/archinstall_2.sh | bash
 
