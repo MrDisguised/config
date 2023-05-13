3661602175234f610e9ea95a2ee3867563278154
MrDisguised <128170542+MrDisguised@users.noreply.github.com>
2023-05-13 08:42:09 +0000

    Update arkenfox-installer.sh

--- arkenfox_firefox/arkenfox-installer.sh
+++ arkenfox_firefox/arkenfox-installer.sh
@@ -6,7 +6,7 @@ rm -rf /home/$(whoami)/.mozilla/firefox/*.default/*.*
 rm -rf /home/$(whoami)/.cache/mozilla/firefox/*.default/*.*
 cd ~/Desktop && git clone https://github.com/arkenfox/user.js && cd user.js
 cp -r *.* /home/$(whoami)/.mozilla/firefox/*.default-release/.
-curl https://raw.githubusercontent.com/MrDisguised/config/main/arkenfox_firefox/user-overrides.txt > /home/$(whoami)/.mozilla/firefox/*.default-release/user-overrides.js
+cd /home/$(whoami)/.mozilla/firefox/*.default-release/ && curl https://raw.githubusercontent.com/MrDisguised/config/main/arkenfox_firefox/user-overrides.txt > user-overrides.js
 curl https://raw.githubusercontent.com/arkenfox/user.js/master/scratchpad-scripts/arkenfox-cleanup.js | wl-copy
 echo "NOW THIS BASH SCRIPT WILL OPEN FIREFOX. STUFF HAS BEEN ALREADY COPIED TO UR CLIPBOARD"
 echo "DO CTRL + SHIFT + K AND IT WILL OPEN THE CONSOLE. TYPE allow pasting TO ALLOW PASTING AND PASTE THE STUFF INSIDE WHICH HAS BEEN COPIED TO YOUR CLIPBOARD BY THIS BASH SCRIPT"
