5684c9c5b86b00de588c16169c25138d703e84d1
MrDisguised <128170542+MrDisguised@users.noreply.github.com>
2023-06-09 05:02:17 +0000

    Update archinstall_2.sh

--- archinstall/archinstall_2.sh
+++ archinstall/archinstall_2.sh
@@ -9,7 +9,7 @@ echo "LC_TIME=en_IN.UTF-8" >> /etc/locale.conf
 echo "FONT=ter-224b" >> /etc/vconsole.conf
 echo "archissexy" >> /etc/hostname
 systemctl enable NetworkManager.service
-echo "NOW IT IS TIME TO SET YOUR ROOT PASSWORD BE VERY CAREFULL AND REMEMBER THE PASSWORD." 
+echo "NOW IT IS TIME TO SET YOUR ROOT PASSWORD BE VERY CAREFULL AND REMEMBER THE PASSWORD!!!!" 
 sleep 6
 passwd
 grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
