5c8a8c871499b8f09b4433c4a45836228ae4f509
Rounak Dutta <128170542+MrDisguised@users.noreply.github.com>
2023-06-26 09:04:49 +0000

    Update archinstall_3.sh

--- archinstall/archinstall_3.sh
+++ archinstall/archinstall_3.sh
@@ -4,4 +4,9 @@ echo "NOW GIVE THE PASSWORD FOR UR NEW USER. BE VERY CAREFULL AND REMEMBER THE P
 sleep 7
 passwd rounak
 usermod -aG wheel,audio,disk,input,kvm,optical,storage,video,libvirt,realtime,rounak rounak
-pacman -S sudo
+pacman -S sudo polkit
+echo "NOW YOU ARE GOING TO EDIT TO SUDOERS FILE. BE VERY CAREFULL!!!! MAKE SURE TO UNCOMMENT THE '%wheel      ALL=(ALL:ALL) ALL' LINE."
+sleep 12
+EDITOR=vim visudo
+echo "ALL DONE :) !! NOW REBOOT YOUR SYSTEM AND RUN THE 'archinstall_4.sh' script"
+
