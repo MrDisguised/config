db2530834e71c85079d0748be88a0f5387d2a361
Rounak Dutta <128170542+MrDisguised@users.noreply.github.com>
2023-06-26 08:58:38 +0000

    Update archinstall_3.sh

--- archinstall/archinstall_3.sh
+++ archinstall/archinstall_3.sh
@@ -3,5 +3,5 @@ useradd -m rounak
 echo "NOW GIVE THE PASSWORD FOR UR NEW USER. BE VERY CAREFULL AND REMEMBER THE PASSWORD!!!!"
 sleep 7
 passwd rounak
-usermod -aG root,wheel,audio,disk,input,kvm,optical,storage,video,libvirt,gamemode,realtime,rounak rounak
+usermod -aG wheel,audio,disk,input,kvm,optical,storage,video,libvirt,realtime,rounak rounak
 pacman -S sudo
